4b1f3e870ec151feeffcfa402f58f37d62ba7132
Luca Piccinelli <luca.picci@gmail.com>
2021-05-10 11:54:11 +0200

    naif green implementation

--- src/test/kotlin/cgm/experiments/hexagonal/GreetingsTests.kt
+++ src/test/kotlin/cgm/experiments/hexagonal/GreetingsTests.kt
@@ -1,8 +1,11 @@
 package cgm.experiments.hexagonal
 
 import io.kotest.matchers.shouldBe
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class GreetingsTests {
 
@@ -24,6 +27,38 @@ class GreetingsTests {
 }
 
 object GreeterService {
-    fun greet(csvSource: String, today: LocalDate = LocalDate.now()): List<String> = emptyList()
+    fun greet(csvSource: String, today: LocalDate = LocalDate.now()): List<String> {
+        val csvFormat = CSVFormat.EXCEL.withDelimiter(',').withHeader()
+        val csv = CSVParser.parse(csvSource, csvFormat)
+
+        val allPersons = csv.map { csvRecord ->
+            val firstName = csvRecord[1].trim()
+            val lastName = csvRecord[0].trim()
+            val dateOfBirth = LocalDate.parse(csvRecord[2].trim(), DateTimeFormatter.ofPattern("yyyy/MM/dd"))
+            val email = csvRecord[3].trim()
+
+            Person(firstName, lastName, dateOfBirth, email)
+        }
+
+        val allBirthdays = allPersons.filter { person ->
+            person.dateOfBirth.month == today.month && person.dateOfBirth.dayOfMonth == today.dayOfMonth
+        }
+
+        val birthdayMessages = allBirthdays.map { person ->
+            """
+                Subject: Happy birthday!
+    
+                Happy birthday, dear ${person.firstName}!
+            """.trimIndent()
+        }
+
+        return birthdayMessages
+    }
 
 }
+
+data class Person(
+    val firstName: String,
+    val lastName: String,
+    val dateOfBirth: LocalDate,
+    val email: String)
